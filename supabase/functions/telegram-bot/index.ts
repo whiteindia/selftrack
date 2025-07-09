f74417ba6515e4141ca3e09cbd0027c04dc9dfa5
wiyuga <44395969+wiyuga@users.noreply.github.com>
2025-07-09 12:10:31 +0530

    telegram fucntoions

--- supabase/functions/telegram-bot/index.ts
+++ supabase/functions/telegram-bot/index.ts
@@ -58,11 +58,7 @@ const handler = async (req: Request): Promise<Response> => {
   const supabase = createClient(
     Deno.env.get('SUPABASE_URL') ?? '',
     Deno.env.get('SUPABASE_ANON_KEY') ?? '',
-    {
-      global: {
-        headers: { Authorization: req.headers.get('Authorization')! },
-      },
-    }
+   
   );
 
   try {
